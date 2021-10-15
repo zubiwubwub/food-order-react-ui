4fa0e539d093eedb8c59a132a31a4e312535d883
Zubiwubwub <zubenkoruslan@gmail.com>
2021-10-15 13:51:15 +0300

    Context set

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,14 +4,18 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const ctx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Total Amount</span>
-      <span className={classes.badge}>{ctx.items.length}</span>
+      <span>{cartCtx.totalAmount < 0 ? `$${cartCtx.totalAmount}` : ''}</span>
+      <span className={classes.badge}>{numberOfItems}</span>
       <span></span>
     </button>
   );
