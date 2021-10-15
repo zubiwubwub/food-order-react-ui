a0b687a433f8a5e1ac6d7f2d212feefe0a414762
Zubiwubwub <zubenkoruslan@gmail.com>
2021-10-15 13:27:21 +0300

    Managing Cart & Modal State

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,10 +3,11 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
+      <span>Total Amount</span>
       <span className={classes.badge}>3</span>
       <span></span>
     </button>
